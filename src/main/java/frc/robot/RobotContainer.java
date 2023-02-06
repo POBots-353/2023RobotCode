a78e5fd761f3a69ef6f845afab040f5e40b3d0cb
programming353 <58999047+programming353@users.noreply.github.com>
2023-02-06 16:46:28 -0500

    Actuator testing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -61,6 +61,7 @@ public class RobotContainer {
 
   private Servo leftServo = new Servo(0);
   private Servo rightServo = new Servo(1);
+  private Servo middleServo = new Servo(2);
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -74,15 +75,18 @@ public class RobotContainer {
 
     leftServo.setBounds(2, 0, 0, 0, 1);
     rightServo.setBounds(2, 0, 0, 0, 1);
+    middleServo.setBounds(2, 0, 0, 0, 1);
 
     driverController.a().whileTrue(Commands.run(() -> {
       leftServo.set(1);
       rightServo.set(1);
+    middleServo.set(1);
     }));
 
     driverController.a().whileFalse(Commands.run(() -> {
       leftServo.set(0);
       rightServo.set(0);
+      middleServo.set(0);
     }));
 
     // driveSubsystem.setDefaultCommand(

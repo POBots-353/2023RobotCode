e856edcada47bbb4ec07d699ca8f532085e3406b
Gold872 <thethunder428@gmail.com>
2023-05-01 15:10:35 -0400

    Added data log manager

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,8 @@ package frc.robot;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.net.PortForwarder;
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -33,8 +35,8 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    // DataLogManager.start();
-    // DriverStation.startDataLog(DataLogManager.getLog(), true);
+    DataLogManager.start();
+    DriverStation.startDataLog(DataLogManager.getLog());
 
     // 160, 120
     // 320, 240
